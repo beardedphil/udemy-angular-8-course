4d5a20cf391fa8c59d3751e3ca53f13100a2a9ac
Phil King <phil@beardedphil.com>
2020-01-30 15:21:53 -0600

    End of Assignment 3 and Section 2.

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  buttonToggleText = 'Display';
+  showDetails = false;
+  buttonClicks = [];
+
+  onToggleDetails() {
+    this.buttonToggleText = this.showDetails ? 'Display' : 'Hide';
+    this.showDetails = !this.showDetails;
+    this.buttonClicks.push(new Date());
+  }
+
+  getBackgroundColor(index) {
+    return index > 3 ? 'blue' : 'transparent';
+  }
 }
